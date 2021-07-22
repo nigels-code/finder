55bbcb9d79ee55a74656693fd1d831688dfc6f57
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-07-22 23:53:37 +0100

    Add Not Found page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className="lead">These are not the pages you're looking for...</p>
+    </div>
+  );
+};
+
+export default NotFound;
